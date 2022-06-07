af34c225ee08bf3f18d1f036456608abfa21c7af
va9ty <reddynishu0@gmail.com>
2022-06-07 16:28:24 -0400

    first commit

---
+++ src/app/overflow-form/overflow-form.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { OverflowFormComponent } from './overflow-form.component';
+
+describe('OverflowFormComponent', () => {
+  let component: OverflowFormComponent;
+  let fixture: ComponentFixture<OverflowFormComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ OverflowFormComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(OverflowFormComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/overflow-form/overflow-form.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-overflow-form',
+  templateUrl: './overflow-form.component.html',
+  styleUrls: ['./overflow-form.component.css']
+})
+export class OverflowFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onSubmit():void{
+    
+  }
+}
